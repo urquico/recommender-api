f0347b6f9f1e92a9f63ff5ecd0a7b0f1722ebcb0
kurturquico <kurt.urquico@simplevia.com>
2025-02-02 22:03:20 +0800

    feature: added more data

--- fake-gen.py
+++ fake-gen.py
@@ -11,13 +11,14 @@ for i in range(2, 2101):
     city = fake.city()
     gender = fake.random_element(elements=("male", "female"))
     age = fake.random_int(min=18, max=80)
+    avatar = fake.image_url()
     
     # save to a csv file
     # create a users folder
     Path(f"users").mkdir(parents=True, exist_ok=True)	
     
     with open(f"users/user_{i}.csv", "w") as f:
-        f.write("id,name,email,created_at,updated_at,city,country,gender,age\n")
-        f.write(f"{i},{name},{email},{created_at},{updated_at},{city},{country},{gender},{age}\n")
+        f.write("id,name,email,created_at,updated_at,city,country,gender,age,avatar\n")
+        f.write(f"{i},{name},{email},{created_at},{updated_at},{city},{country},{gender},{age},{avatar}\n")
     
     
